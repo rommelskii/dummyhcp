9edb2f6dcc431925e72bfd075c25bc0e76da0c00
Rommel John Ronduen <rommel.ronduen2244@gmail.com>
2026-01-18 03:12:40 +0800

    added basic test fixtures

---
+++ tests/fixtures/packet_fixtures.h
@@ -0,0 +1,36 @@
+#ifndef PACKET_FIXTURES_H
+#define PACKET_FIXTURES_H
+
+#include "packet.h"
+#include <gtest/gtest.h>
+
+class PacketInitialization : public ::testing::Test {
+protected:
+  dhcp_packet dp{};
+};
+
+class PacketHeaderBuild : public ::testing::Test {
+protected:
+  dhcp_packet dp;
+  const uint8_t     op            = 1;     
+  const uint8_t     htype         = 1;   
+  const uint8_t     hlen          = 6;     
+  const uint8_t     hops          = 0;      
+  const uint32_t    xid           = 0xDEADBEEF;        
+  const uint16_t    secs          = 2600;        
+  const uint16_t    flags         = 0;       
+  const uint32_t    ciaddr        = inet_addr("1.0.0.0");      
+  const uint32_t    yiaddr        = inet_addr("0.1.0.0");    
+  const uint32_t    siaddr        = inet_addr("0.0.1.0");     
+  const uint32_t    giaddr        = inet_addr("192.168.100.69");      
+  uint8_t     chaddr[16]    = {0};   
+  uint8_t     sname[64]     = {0};     
+  uint8_t     file[128]     = {0};      
+  const uint32_t    magic_cookie  = 0xAAAAFFFF;   
+
+  void SetUp() override {
+    dp.build_header(op, xid, secs, flags, ciaddr, yiaddr, siaddr, giaddr, chaddr);
+  }
+};
+
+#endif
--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -1,7 +1,68 @@
 #include <gtest/gtest.h>
 #include "packet.h"
+#include "arpa/inet.h"
+#include "packet_fixtures.h"
 
-TEST(Packet, PacketCreation) {
-  EXPECT_STRNE("hello", "world");
-  EXPECT_EQ(7 * 6, 42);
+TEST_F(PacketInitialization, PacketInitializationCheck) {
+  EXPECT_EQ(dp.header.op, 0);
+  EXPECT_EQ(dp.header.htype, 0);
+  EXPECT_EQ(dp.header.hlen, 0);
+  EXPECT_EQ(dp.header.hops, 0);
+  EXPECT_EQ(dp.header.xid, 0);
+  EXPECT_EQ(dp.header.secs, 0);
+  EXPECT_EQ(dp.header.flags, 0);
+  EXPECT_EQ(dp.header.ciaddr, 0);
+  EXPECT_EQ(dp.header.yiaddr, 0);
+  EXPECT_EQ(dp.header.siaddr, 0);
+  EXPECT_EQ(dp.header.giaddr, 0);
+  for ( auto x : dp.header.chaddr ) {
+    EXPECT_EQ(x, 0);
+  }
+  for ( auto x : dp.header.sname ) {
+    EXPECT_EQ(x, 0);
+  }
+  for ( auto x : dp.header.file ) {
+    EXPECT_EQ(x, 0);
+  }
+  EXPECT_EQ(dp.header.magic_cookie, 0);
+}
+
+TEST_F(PacketHeaderBuild, PacketHeaderOpCheck) {
+  EXPECT_EQ(dp.header.op, op);
+}
+TEST_F(PacketHeaderBuild, PacketHeaderXidCheck) {
+  EXPECT_EQ(dp.header.xid, xid);
+}
+TEST_F(PacketHeaderBuild, PacketHeaderSecsCheck) {
+  EXPECT_EQ(dp.header.secs, secs);
+}
+TEST_F(PacketHeaderBuild, PacketHeaderFlagsCheck) {
+  EXPECT_EQ(dp.header.flags, flags);
+}
+TEST_F(PacketHeaderBuild, PacketHeaderCiaddrCheck) {
+  EXPECT_EQ(dp.header.ciaddr, ciaddr);
+}
+TEST_F(PacketHeaderBuild, PacketHeaderYiaddrCheck) {
+  EXPECT_EQ(dp.header.yiaddr, yiaddr);
+}
+TEST_F(PacketHeaderBuild, PacketHeaderSiaddrCheck) {
+  EXPECT_EQ(dp.header.siaddr, siaddr);
+}
+TEST_F(PacketHeaderBuild, PacketHeaderGiaddrCheck) {
+  EXPECT_EQ(dp.header.giaddr, giaddr);
+}
+TEST_F(PacketHeaderBuild, PacketHeaderChaddrCheck) {
+  for(int i=0; i<sizeof(dp.header.chaddr); ++i) {
+    EXPECT_EQ(dp.header.chaddr[i], chaddr[i]);
+  }
+}
+TEST_F(PacketHeaderBuild, PacketHeaderSnameCheck) {
+  for(int i=0; i<sizeof(dp.header.sname); ++i) {
+    EXPECT_EQ(dp.header.sname[i], sname[i]);
+  }
+}
+TEST_F(PacketHeaderBuild, PacketHeaderFileCheck) {
+  for(int i=0; i<sizeof(dp.header.file); ++i) {
+    EXPECT_EQ(dp.header.file[i], file[i]);
+  }
 }
